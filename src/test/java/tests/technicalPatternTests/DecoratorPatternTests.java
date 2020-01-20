45e7883305613c68c2a936214e2f7e023c8eb4d4
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-01-20 12:48:47 +0300

    add Decorator pattern tests

--- src/test/java/tests/technicalPatternTests/DecoratorPatternTests.java
+++ src/test/java/tests/technicalPatternTests/DecoratorPatternTests.java
@@ -1,34 +1,37 @@
 package tests.technicalPatternTests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utils.decoratorUtils.*;
 
 public class DecoratorPatternTests {
+    private RegisterUser userJulia;
+    private RegisterUser userTomasz;
+    private RegisterUser userPetr;
+
+    @BeforeTest
+    public void setUp() {
+        userJulia = new Julia();
+        userTomasz = new Tomasz();
+        userPetr = new Petr();
+    }
 
     @Test
-    public void test_1() {
-        RegisterUser userJulia = new Julia();
-//        System.out.print(userJulia.getUserInfo());
-//        userJulia = new Address(userJulia);
-//        System.out.print(userJulia.getUserInfo());
-//        userJulia = new Contacts(userJulia);
-//        System.out.print(userJulia.getUserInfo());
-
+    public void testUserInfo_Julia() {
         RegisterUser userJulia2 = new Address(new Contacts(userJulia));
         System.out.print(userJulia2.getUserInfo());
-
-
     }
 
     @Test
-    public void test_2() {
-        RegisterUser userTomasz = new Tomasz();
+    public void testUserInfo_Tomasz() {
         RegisterUser userTomasz2 = new Address(new Contacts(userTomasz));
         System.out.print(userTomasz2.getUserInfo());
-
-
     }
 
-
+    @Test
+    public void testUserInfo_Petr() {
+        RegisterUser userPetr2 = new Address(new Contacts(userPetr));
+        System.out.print(userPetr2.getUserInfo());
+    }
 
 }
